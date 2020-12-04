cfef07198029c5fa15cb733d3630ab13d8ebc61a
Kirill <kiruxastar@gmail.com>
2020-12-04 12:38:40 +0300

    Tests updated

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/k1eub9324gt1r60f?svg=true)](https://ci.appveyor.com/project/kirmakin/aqa-2-1)
--- src/test/java/ru/netology/WebpageTest.java
+++ src/test/java/ru/netology/WebpageTest.java
@@ -1,8 +1,6 @@
 package ru.netology;
-import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
+import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -11,19 +9,12 @@ public class WebpageTest {
     @Test
     public void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Иван Сидоров");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[data-test-id=submit]").click();
-        $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена!"));
+//        SelenideElement form = $("[data-test-id=callback-form]");
+        $("[data-test-id=name] input").setValue("Иван Сидоров");
+        $("[data-test-id=phone] input").setValue("+79270000001");
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $(".form-field .button__content").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
         }
-//        driver.get("http://localhost:9999");
-//        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Сидоров");
-//        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79881112233");
-//        driver.findElement(By.cssSelector("[data-test-id=agreement] .checkbox__box")).click();
-//        driver.findElement(By.cssSelector(".form-field .button__content")).click();
-//        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-//        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
-//    }
+
 }
