ff547018ca9b32386745fb64611e93369fdbf2d1
Kirill <kiruxastar@gmail.com>
2020-12-04 22:30:28 +0300

    Valiation Test added

--- src/test/java/ru/netology/WebpageTest.java
+++ src/test/java/ru/netology/WebpageTest.java
@@ -9,12 +9,20 @@ public class WebpageTest {
     @Test
     public void shouldSubmitRequest() {
         open("http://localhost:9999");
-//        SelenideElement form = $("[data-test-id=callback-form]");
         $("[data-test-id=name] input").setValue("Иван Сидоров");
         $("[data-test-id=phone] input").setValue("+79270000001");
         $("[data-test-id=agreement] .checkbox__box").click();
         $(".form-field .button__content").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
         }
-
+        
+    @Test
+    public void shouldntSubmitRequestByName() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Ivan Сидоров");
+        $("[data-test-id=phone] input").setValue("+79270000001");
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $(".form-field .button__content").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
 }
